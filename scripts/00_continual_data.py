6e1b2a3c77d2a75d377e604012164092aca32a72
Vicente <vvcastro@uc.cl>
2024-05-28 12:04:27 +0200

    Correct splitting of datasets, transformation to training data

--- scripts/00_continual_data.py
+++ scripts/00_continual_data.py
@@ -5,7 +5,7 @@ from src.evaluator.model_trainer import OFAModelTrainer
 
 from src.evaluator.evaluator import OFAEvaluator
 from src.data.preparation import (
-    prepare_continual_splits,
+    splits_continual_data,
     transform_dataset,
 )
 
@@ -24,16 +24,18 @@ import torch.nn as nn
 DynamicSeparableConv2d.KERNEL_TRANSFORM_MODE = 1
 
 
+search_space = OFASearchSpace(family="mobilenetv3")
 ofa_net = OFAEvaluator(
     family="mobilenetv3",
     model_path=OFA_MODEL_PATH,
     pretrained=True,
-    data_classes=2,
+    data_classes=10,
 )
 
 # Step 0: Sample an architecture
-search_space = OFASearchSpace(family="mobilenetv3")
-sampled_architecture = search_space.sample(n_samples=1)[0]
+sampled_architecture = search_space._get_min_sample()
+# sampled_architecture = search_space._get_max_sample()
+# sampled_architecture = search_space.sample(n_samples=1)[0]
 model, _ = ofa_net.get_architecture_model(sampled_architecture)
 
 # Step 1. Load CIFAR-10 train/test dataset
@@ -42,20 +44,22 @@ train_dataset = datasets.CIFAR10(root=CIFAR10_DATA_DIR, train=True, download=Tru
 test_dataset = datasets.CIFAR10(root=CIFAR10_DATA_DIR, train=False, download=True)
 
 # Step 2. Add the same transformation to the dataset
-train_dataset = transform_dataset(train_dataset, sampled_architecture["resolution"])
-prepared_dataset = transform_dataset(test_dataset, sampled_architecture["resolution"])
+testing_dataset = transform_dataset(test_dataset, sampled_architecture["resolution"])
+training_dataset = transform_dataset(
+    train_dataset,
+    sampled_architecture["resolution"],
+    train=True,
+)
 
 # Step 3: Select specific classes for continual learning
-SPLIT_SIZES = [0.5, 0.5]
-train_datasets = prepare_continual_splits(
-    train_dataset,
+SPLIT_SIZES = [0.5, 0.3, 0.2]
+training_datasets = splits_continual_data(
+    training_dataset,
     split_sizes=SPLIT_SIZES,
-    classes=[0, 1],
 )
-test_datasets = prepare_continual_splits(
-    test_dataset,
+testing_datasets = splits_continual_data(
+    testing_dataset,
     split_sizes=SPLIT_SIZES,
-    classes=[0, 1],
 )
 
 
@@ -64,10 +68,20 @@ print("(2) Training the model...")
 metrics = {"accuracy": binary_accuracy}
 trainer = OFAModelTrainer(model, custom_metrics=metrics)
 
-optimiser = optim.Adam(model.parameters())
+
+##### PARAMS TO VARY
+## batch_size = 64
+## learning_rate = 1e-3
+
+BATCH_SIZE = 64
+LEARNING_RATE = 1e-3
+
+base_optimiser = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+optimiser = optim.lr_scheduler.CosineAnnealingLR(base_optimiser)
+
 criterion = nn.CrossEntropyLoss()
 
-for train_set, test_set in zip(train_datasets, test_datasets):
+for train_set, test_set in zip(training_datasets, testing_datasets):
     # Create DataLoaders
     train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
     val_loader = DataLoader(test_set, batch_size=64, shuffle=False)
--- src/data/preparation.py
+++ src/data/preparation.py
@@ -8,37 +8,27 @@ from typing import List, Tuple
 DatasetType = datasets.CIFAR10 | datasets.CIFAR100
 
 
-def prepare_continual_splits(
+def splits_continual_data(
     dataset: DatasetType, split_sizes: List[float], classes: List[int] = None
 ) -> List[Subset]:
     """
-    Split the dataset into multiple subsets based on the given sizes and optionally filter by classes.
+    Split the dataset into multiple disjoint subsets based on the given percentual sizes.
 
     Args:
     dataset (CIFAR10): The dataset to split.
     split_sizes (List[float]): The percentual sizes, from the original dataset, to give to each split.
-    classes (List[int], optional): List of class indices to include. Defaults to None.
 
     Returns:
     List[Subset]: List of filtered and split subsets.
     """
-    if classes is not None:
-        # Filter the dataset by the specified classes
-        class_indices = [
-            i for i, label in enumerate(dataset.targets) if label in classes
-        ]
-        filtered_dataset = Subset(dataset, class_indices)
-    else:
-        filtered_dataset = dataset
-
-    targets = [filtered_dataset[i][1] for i in range(len(filtered_dataset))]
-
     # Ensure the split sizes sum to 1.0
     split_sizes = [size / sum(split_sizes) for size in split_sizes]
 
+    # For stratified splits.
+    targets = [dataset[i][1] for i in range(len(dataset))]
+
     subsets = []
     remaining_indices = list(range(len(targets)))
-
     for split_size in split_sizes[:-1]:
         split_length = int(len(targets) * split_size)
         split_indices, remaining_indices = train_test_split(
@@ -47,13 +37,17 @@ def prepare_continual_splits(
             stratify=[targets[i] for i in remaining_indices],
             random_state=42,
         )
-        subsets.append(Subset(filtered_dataset, split_indices))
+        subsets.append(Subset(dataset, split_indices))
 
-    subsets.append(Subset(filtered_dataset, remaining_indices))
+    subsets.append(Subset(dataset, remaining_indices))
     return subsets
 
 
-def transform_dataset(dataset: DatasetType, resolution: Tuple[int, int]) -> DatasetType:
+def transform_dataset(
+    dataset: DatasetType,
+    resolution: Tuple[int, int],
+    train: bool = False,
+) -> DatasetType:
     """
     Prepare the dataset by applying transformations.
 
@@ -64,11 +58,22 @@ def transform_dataset(dataset: DatasetType, resolution: Tuple[int, int]) -> Data
     Returns:
     datasets.CIFAR10: Transformed dataset.
     """
-    transform = transforms.Compose(
+
+    # Default values for CIFAR10
+    norm_mean = [0.49139968, 0.48215827, 0.44653124]
+    norm_std = [0.24703233, 0.24348505, 0.26158768]
+
+    transform = []
+    if train:
+        transform = [
+            transforms.RandomResizedCrop(resolution, scale=(0.08, 1)),
+            transforms.RandomHorizontalFlip(p=0.5),
+        ]
+
+    transform.extend(
         [
-            transforms.Resize(resolution),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            transforms.Normalize(mean=norm_mean, std=norm_std),
         ]
     )
     dataset.transform = transform
